abbdb48a6ccd19a37fb3d0e08684a6b5c1694a58
VeroniqueDM <veronika.dmiteva@gmail.com>
2023-08-16 10:38:03 +0200

    extracted the components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import NavLeft from "./NavLeft";
+import NavRight from "./NavRight";
+
+function Header() {
+    return (
+        <header>
+        <NavLeft/>
+        <NavRight/>       
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/NavLeft.js
@@ -0,0 +1,10 @@
+function NavLeft () {
+return (<nav className="left-menu">
+<ul>
+  <li>About</li>
+  <li>Store</li>
+</ul>
+</nav>);
+}
+
+export default NavLeft;
