a29dcc2d53ffd42dcd4f051bdd828d42773235df
Ntina B <n_bekiari@outlook.com>
2023-08-16 17:11:20 +0300

    ok .

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p className="country">United Kingdom</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
