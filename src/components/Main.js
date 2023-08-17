da812211a7daaf9dfcc4bf9230a6e83f0e48260e
Georgopoulos Giorgos <dgiorgos90@gmail.com>
2023-08-17 09:47:18 +0300

    mu first react exercise

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+
+function Main() {
+  return (
+    <main>
+      <p>This is the main content of the app.</p>
+    </main>
+  );
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 import App from './App'
 import './styles.css'
@@ -8,6 +11,9 @@ const rootElement = document.getElementById('root')
 ReactDOM.render(
   <StrictMode>
     <App />
+    <Header />
+      <Main />
+      <Footer />
   </StrictMode>,
   rootElement
 )
