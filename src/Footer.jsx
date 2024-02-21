0ca3fd33b44233fed4151b771ded1956d9358318
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-02-21 09:46:56 +0100

    Finished exercise

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+function Bottom()
+{   
+    return (
+        <footer>
+        <p className="country">United Kingdom</p>
+      </footer>
+    )
+}
+
+export default Bottom
