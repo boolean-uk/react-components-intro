e1d3c84dff3ceb0275296412e11f5e892fa23a63
Victor <victor.adamson@se.experis.com>
2024-02-21 10:10:07 +0100

    Extracted more things

---
+++ src/Left-menu.jsx
@@ -0,0 +1,13 @@
+function LeftMenu() {
+  return (
+    <>
+      <nav className="left-menu">
+        <ul>
+          <li>About</li>
+          <li>Store</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+export default LeftMenu;
