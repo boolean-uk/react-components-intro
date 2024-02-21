bac5a023d03fe4378224e8e7300a19792b108702
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-02-21 10:27:37 +0100

    Seperate code into different componentes core + extension
    Many more components can be done but there is no need for cutting it into more pieces

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LeftMenu from "./LeftMenu";
+import RightMenu from "./RightMenu";
+
+function Header() {
+  return (
+    <header>
+      <LeftMenu />
+      <RightMenu />
+    </header>
+  );
+}
+
+export default Header;
