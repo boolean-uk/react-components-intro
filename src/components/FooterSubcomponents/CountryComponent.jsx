d34f6aa4c67560206b5002c758794b5eb96b73da
Benjamin A. E. Darici <benjamin.darici@no.experis.com>
2024-02-22 09:04:58 +0100

    Finished Core and Extension I think

---
+++ src/components/FooterSubcomponents/CountryComponent.jsx
@@ -0,0 +1,7 @@
+function CountryComponent(){
+    return (
+      <p className="country">United Kingdom</p>
+    )
+}
+
+export default CountryComponent
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import LeftMenuComponent from "./HeaderSubcomponents/LeftMenuComponent";
+import RightMenuComponent from "./HeaderSubcomponents/RightMenuComponent";
+function HeaderComponent(){
+    return (
+    <header> 
+    <LeftMenuComponent />
+    <RightMenuComponent />
+  </header>
+  )
+}
+
+export default HeaderComponent;
