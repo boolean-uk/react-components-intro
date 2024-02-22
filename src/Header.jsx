068293346210e0efedf2b13b585f6cba51aa0450
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2024-02-22 12:11:02 +0100

    core and extentions

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import LeftHeader from "./LeftHeader";
+import RightHeader from "./RightHeader";
+function Header(){
+    return(
+      <header>
+        <LeftHeader />
+        <RightHeader />
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/LeftHeader.jsx
@@ -0,0 +1,12 @@
+function LeftHeader() {
+  return (
+    <nav className="left-menu">
+          <ul>
+            <li>About</li>
+            <li>Store</li>
+          </ul>
+        </nav>
+  )
+}
+
+export default LeftHeader
