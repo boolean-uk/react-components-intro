0d74f42217106a8fdd75ab520624b791e5955bd8
oysteinbjo <oystein.bjorkeng@no.experis.com>
2024-02-22 10:18:37 +0100

    core and extensions done

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import LeftMenu from './LeftMenu';
+import RightMenu from './RightMenu';
+
+const Header = () => {
+    return (
+        
+        <header>
+        <LeftMenu/>
+        <RightMenu/>
+
+        
+        </header>
+
+    );
+}
+
+export default Header;
