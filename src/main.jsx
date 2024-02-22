4b7df2edf4e0e75bd6470b875f11467ebc9ef7f7
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-02-22 15:00:46 +0100

    Completed Core

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Header from './Header.jsx'
 import App from './App.jsx'
+import Footer  from './Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <Header/>
+      <App />
+      <Footer />
+    </div>
   </React.StrictMode>,
 )
