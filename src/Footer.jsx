a42705cb6918eff4529104c7fdfe5d3bcdd60d84
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-02-22 15:39:32 +0100

    Split .css file. Completed Extension.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './Styles/Footer.css'
 
 const Footer = () => {
     return (
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './Styles/Header.css'
 
 const Header = () => {
     return (
