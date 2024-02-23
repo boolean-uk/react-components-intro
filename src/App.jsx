bf1501a1e8a9036d73a2bdb6e54677b1a6c0b064
Stian Andreas Ytterstad Nordvik <stian.andreas.ytterstad.nordvik@no.experis.com>
2024-02-23 09:18:36 +0100

    Stian Andreas Ytterstad Nordvik C#3

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,78 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <header>
-        <nav className="left-menu">
+    <HeaderComponent />
+    <MainComponent />
+    <FooterComponent />
+    </div>
+  )
+}
+
+function HeaderComponent () 
+{
+  return (
+    <header>
+      <LeftMenuComponent />
+      <RightMenuComponent />
+    </header>
+  )
+}
+
+function LeftMenuComponent () 
+{
+  return (
+    <nav className="left-menu">
           <ul>
             <li>About</li>
             <li>Store</li>
           </ul>
-        </nav>
+    </nav>
+  )
+}
 
-        <nav className="right-menu">
+function RightMenuComponent () 
+{
+  return (
+    <nav className="right-menu">
           <ul>
             <li>Gmail</li>
             <li>Images</li>
             <li>
-              <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
-                <path
-                  d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
-              </svg>
+              <MenuIconComponent />
             </li>
             <li>
-              <img
-                src="https://lh3.google.com/u/0/ogw/ADGmqu9SnLFSXSNhQekSAI8qQNn9h-VAhGGPPtNZefIQ=s64-c-mo"
-                alt="avatar"
-                className="avatar"
-              />
+              <AvatarComponent />
             </li>
           </ul>
-        </nav>
-      </header>
+     </nav>
+  )
 
-      <main>
+}
+
+function MenuIconComponent () {
+  return (
+    <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
+        <path
+          d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+    </svg>
+  )
+}
+function AvatarComponent () {
+  return (
+    <img
+        src="https://lh3.google.com/u/0/ogw/ADGmqu9SnLFSXSNhQekSAI8qQNn9h-VAhGGPPtNZefIQ=s64-c-mo"
+        alt="avatar"
+        className="avatar"
+    />
+  )
+
+}
+
+
+function MainComponent () 
+{
+  return (
+    <main>
         <img
           className="logo"
           src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
@@ -44,11 +86,16 @@ function App() {
           <button>Feeling Lucky</button>
         </div>
       </main>
+  )
 
-      <footer>
-        <p className="country">United Kingdom</p>
-      </footer>
-    </div>
+}
+
+function FooterComponent () 
+{
+  return (
+    <footer>
+      <p className="country">United Kingdom</p>
+    </footer>
   )
 }
 
