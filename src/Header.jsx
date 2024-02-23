d1798ea759471c8c7df591b8402d8293d72cac82
Eddy1108 <edvardh@hotmail.no>
2024-02-23 13:51:19 +0100

    Core + Extension

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import HeaderLeft from "./HeaderLeft"
+import HeaderRight from "./HeaderRight"
+
+function HeaderTop () {
+return <> <header>
+<HeaderLeft />
+
+<HeaderRight />
+</header>
+</>
+}
+
+export default HeaderTop
---
+++ src/MainContentButtons.jsx
@@ -0,0 +1,8 @@
+function MainButtons (){
+    return     <div className="buttons">
+    <button>Google Search</button>
+    <button>Feeling Lucky</button>
+  </div>
+}
+
+export default MainButtons
