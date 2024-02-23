e7b95a11c29dc5c326aaa49df572047d8637e609
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-02-23 11:20:28 +0100

    Done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+
+
+class Footer extends React.Component {
+    render() {
+      return(
+        <footer>
+        <p className="country">United Kingdom</p>
+      </footer>
+      ) 
+      
+      }
+    }
+
+    export default Footer;
