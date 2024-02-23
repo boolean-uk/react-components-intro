a8fcdd9ed0bcd8f77e2feb63771156ec1f05b9d5
SBjer1 <sebastian.engan@no.experis.com>
2024-02-23 10:24:56 +0100

    Sebastian Engan

---
+++ src/Components/Buttons.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Buttons() {
+  return (
+    <>
+        <div className="buttons">
+          <button>Google Search</button>
+          <button>Feeling Lucky</button>
+        </div>
+    </>
+  )
+}
+
+export default Buttons
