42574963802c3f1b98155e3c73aa18f287a667d2
Louis <louisshrestha0412@outlook.com>
2024-02-23 13:02:34 +0100

    tasks completed

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button"
+export default function Main(){
+    return (
+        <main>
+        <img
+          className="logo"
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt="Google logo"
+        />
+        <input className="search-bar" type="search" />
+        <Button/>
+      </main>
+    )
+}
