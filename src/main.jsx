ab4ebdbdb66fd402a8f2f5f93f98a33dd89b6367
Sebastian <sebastian.hanssen@outlook.com>
2024-02-26 11:39:23 +0100

    finish exercise

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import HeaderComponent from './HeaderComponent.jsx'
+import MainComponent from './MainComponent.jsx'
+import FooterComponent from './FooterComponent.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+    <HeaderComponent/>
+    <MainComponent/>
+    <FooterComponent/>
+    </div>
   </React.StrictMode>,
 )
